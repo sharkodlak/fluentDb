64b98f20ced785a66774a7c1ff93260c7f109eef
Pavel Stetina <github@shark.kom.cz>
2016-03-05 00:56:26 +0100

    Test method Db->query

--- tests/DbTest.php
+++ tests/DbTest.php
@@ -16,6 +16,23 @@ class DbTest extends \PHPUnit_Framework_TestCase {
 		$this->assertInstanceOf(Table::class, $db->film);
 	}
 
+	public function testQuery() {
+		$query = 'SELECT * FROM film';
+		$pdo = $this->getMockBuilder('PDO')
+			->disableOriginalConstructor()
+			->getMock();
+		$pdo->expects($this->once())
+			->method('query')
+			->with($this->equalTo($query));
+		$cache = $this->getMockBuilder(\Psr\Cache\CacheItemPoolInterface::class)
+			->getMock();
+		$convention = $this->getMockBuilder(Structure\DefaultConvention::class)
+			->disableOriginalConstructor()
+			->getMock();
+		$db = new Db($pdo, $cache, $convention);
+		$db->query($query);
+	}
+
 
 	public function testGetQuery() {
 		$this->markTestSkipped('Not implemented yet.');
