6052e96b9d4f51f0da184cd2f18c1f48d8041313
Pavel Stetina <github@shark.kom.cz>
2016-03-04 22:45:55 +0100

    Class TableQuery covered with tests

--- tests/Query/TableQueryTest.php
+++ tests/Query/TableQueryTest.php
@@ -5,7 +5,7 @@ namespace Sharkodlak\FluentDb\Query;
 class TableQueryTest extends \PHPUnit_Framework_TestCase {
 	public function test__toString() {
 		$parts = ['SELECT', '*', 'FROM', '%s'];
-		$query = $this->getMockBuilder('Sharkodlak\\FluentDb\\Query\\TableQuery')
+		$query = $this->getMockBuilder(TableQuery::class)
 			->disableOriginalConstructor()
 			->getMockForAbstractClass();
 		$query->expects($this->once())
@@ -22,21 +22,75 @@ class TableQueryTest extends \PHPUnit_Framework_TestCase {
 		$statement->expects($this->once())
 			->method('execute')
 			->will($this->returnValue(true));
-		$table = $this->getMockBuilder('Sharkodlak\\FluentDb\\Table')
+		$table = $this->getMockBuilder(\Sharkodlak\FluentDb\Table::class)
 			->disableOriginalConstructor()
 			->getMock();
 		$table->expects($this->any())
 			->method('query')
 			->will($this->returnValue($statement));
 		$parts = ['SELECT', '*', 'FROM', '%s'];
-		$query = $this->getMockBuilder('Sharkodlak\\FluentDb\\Query\\TableQuery')
+		$query = $this->getMockBuilder(TableQuery::class)
 			->setConstructorArgs([$table])
 			->getMockForAbstractClass();
 		$query->expects($this->any())
 			->method('getParts')
 			->will($this->returnValue($parts));
 		$this->assertFalse($query->isExecuted());
-		$query->executeOnce();
+		$this->assertInstanceOf('PDOStatement', $query->executeOnce());
 		$this->assertTrue($query->isExecuted());
+		$this->expectException(\Sharkodlak\Exception\IllegalStateException::class);
+		$query->executeOnce();
+	}
+
+	public function testGetResult() {
+		$statement = $this->getMockBuilder('PDOStatement')
+			->disableOriginalConstructor()
+			->getMock();
+		$statement->expects($this->once())
+			->method('execute')
+			->will($this->returnValue(true));
+		$table = $this->getMockBuilder(\Sharkodlak\FluentDb\Table::class)
+			->disableOriginalConstructor()
+			->getMock();
+		$table->expects($this->any())
+			->method('query')
+			->will($this->returnValue($statement));
+		$parts = ['SELECT', '*', 'FROM', '%s'];
+		$query = $this->getMockBuilder(TableQuery::class)
+			->setConstructorArgs([$table])
+			->getMockForAbstractClass();
+		$query->expects($this->any())
+			->method('getParts')
+			->will($this->returnValue($parts));
+		$this->assertFalse($query->isExecuted());
+		$this->assertInstanceOf('PDOStatement', $query->getResult());
+		$this->assertTrue($query->isExecuted());
+		$this->assertInstanceOf('PDOStatement', $query->getResult());
+		$this->assertTrue($query->isExecuted());
+	}
+
+	public function testExecutionError() {
+		$statement = $this->getMockBuilder('PDOStatement')
+			->disableOriginalConstructor()
+			->getMock();
+		$statement->expects($this->once())
+			->method('execute')
+			->will($this->returnValue(false));
+		$table = $this->getMockBuilder(\Sharkodlak\FluentDb\Table::class)
+			->disableOriginalConstructor()
+			->getMock();
+		$table->expects($this->any())
+			->method('query')
+			->will($this->returnValue($statement));
+		$parts = ['SELECT', '*', 'FROM', '%s'];
+		$query = $this->getMockBuilder(TableQuery::class)
+			->setConstructorArgs([$table])
+			->getMockForAbstractClass();
+		$query->expects($this->any())
+			->method('getParts')
+			->will($this->returnValue($parts));
+		$this->assertFalse($query->isExecuted());
+		$this->expectException(\Exception::class);
+		$query->executeOnce();
 	}
 }
